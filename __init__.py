1c7eec5f33ba692b33f6721fe86d97868498798a
André Ritter <andre.ritter@fau.de>
2014-11-20 17:32:57 +0100

    Extended pycon.show_doc_html().

--- __init__.py
+++ __init__.py
@@ -13,14 +13,34 @@ Show the documentation with pycon.show_doc_html().
 
 from io import load_ranges
 
-def show_doc_html():
+def show_doc_html(refresh=False):
     '''
-    Show the documentation. This needs a webbrowser to be installed and the html
-    documentation in the doc subdirectory of the pycon package to be built.
+    Show the documentation. This needs a webbrowser to be installed.
+    Additionally the function tries to refresh the documentation if wanted.
+    
+    :param bool refresh: If set to True the function tries to refresh the
+                         documentation before showing it.
     ''' 
     import webbrowser
+    import os
     import os.path
+    import subprocess
     package_root = os.path.dirname(os.path.abspath(__file__))
     html_index = os.path.join(package_root, 'doc/_build/html/index.html')
-    print 'Showing sphinx html documentation at: '+html_index
-    webbrowser.open('file://'+html_index)
+    # Try to update documentation if needed.
+    if refresh:
+        cwd_old = os.getcwd()
+        os.chdir(os.path.join(package_root, 'doc'))
+        if subprocess.call(['make', 'html']) == 0:
+            print 'Html documentation refreshed.'
+        else:
+            print ('Could not refresh html documentation. Maybe sphinx-doc is '
+                   'not installed on this system.')
+        os.chdir(cwd_old)
+    # Try showing documentation.
+    if os.path.exists(html_index):
+        print 'Showing sphinx html documentation at: '+html_index
+        webbrowser.open('file://'+html_index)
+    else:
+        print 'Could not find html documentation index at: '+html_index
+    
