e4183a09b6bbf4e7bedea325bdc0b2a4d17205d0
André Ritter <andre.ritter@fau.de>
2014-11-20 17:00:37 +0100

    Adding pycon.show_doc_html() function.

--- __init__.py
+++ __init__.py
@@ -4,6 +4,23 @@
 #
 # Copyright (C)
 # 2012-2014 André Ritter (andre.ritter@fau.de)
+'''
+This is the pycon Python package. A framework for image reconstruction in X-ray
+Talbot-Lau interferometry.
+
+Show the documentation with pycon.show_doc_html().
+'''
 
 from io import load_ranges
 
+def show_doc_html():
+    '''
+    Show the documentation. This needs a webbrowser to be installed and the html
+    documentation in the doc subdirectory of the pycon package to be built.
+    ''' 
+    import webbrowser
+    import os.path
+    package_root = os.path.dirname(os.path.abspath(__file__))
+    html_index = os.path.join(package_root, 'doc/_build/html/index.html')
+    print 'Showing sphinx html documentation at: '+html_index
+    webbrowser.open('file://'+html_index)
