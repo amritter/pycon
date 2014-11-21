0f9d81b5f28bf22716cfe748f6b4471122014b8d
André Ritter <andre.ritter@fau.de>
2014-11-21 16:09:49 +0100

    Added in pycon.tomo added __init__.py. Added some first documentation.

--- pycon/tomo/__init__.py
+++ pycon/tomo/__init__.py
@@ -5,4 +5,12 @@
 # Copyright (C)
 # 2012-2014 André Ritter (andre.ritter@fau.de)
 '''
+
+2D-tomo coordinates
+
+.. math::
+
+    x = \\lambda\\cdot\\sin(\\theta) + \\xi\\cdot\\cos(\\theta) 
+    
+    y = \\lambda\\cdot\\cos(\\theta) - \\xi\\cdot\sin(\\theta)
 '''
