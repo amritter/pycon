68a14d0022fcbe2d6e5569815a6be6dcc51932d9
André Ritter <andre.ritter@fau.de>
2014-11-28 12:55:20 +0100

    In pycon.tomo: * Added method reco_fbp as simplified interface to methods defined in pycon.tomo.fbp. In pycon.tomo.fbp: * Renamed methods: zero_pad to pad, filter_parallel_projection to filter_projection, backproject_parallel_projection to backproject_projection and backproject_parallel to backproject. * Updated method pad. * Added method unpad.

--- pycon/tomo/__init__.py
+++ pycon/tomo/__init__.py
@@ -14,3 +14,61 @@
     
     y = \\lambda\\cdot\\cos(\\theta) - \\xi\\cdot\sin(\\theta)
 '''
+import fbp
+import numpy
+import pycon.util
+
+def reco_fbp(sino, thetas=None, xis=None, xi_offs=0., xy=None, kernel=None, pads=None, 
+             pad_value=0., interpolation='linear', thetas_axis=-2, xis_axis=-1):
+    '''
+    Reconstruct a cross section from tomographic parallel projection data using
+    the filtered back projection method.
+    
+    :param numpy.ndarray sino: An array containing the sinogram data.
+    :param numpy.ndarray thetas: An array specifing the angle in radians of each
+                                 parallel projection. If set to None, angles are
+                                 distributed between 0 and 2 pi according to the
+                                 number of projections in sino.
+    :param numpy.ndarray xis: An array specyifing the xi values of the pixels in
+                              each parallel projection. If set to None, a pitch
+                              of one is assumed and a number of xis specified by
+                              the shape of sino is distributed around zero.
+    :param float xi_offs: If xis are set to None this value allows to shift the
+                          computed xis by the given offset. This allows to
+                          correct for a not centered rotation axis.
+    :param tuple xy: A tuple of two equal shaped 2d numpy.ndarray objects. The
+                     shape reflects the shape of the reconstruced x-y-volume.
+                     The first array gives the x coordinates the second array
+                     gives the y coordinates. If set to None, a volume is
+                     assumed which is defined by ``max(abs(xis))`` for its outer
+                     boundaries and ``max(diff(xis))'' for its voxel size.
+    :param kernel: A kernel object accepted by pycon.tomo.filter_projection
+                   method.
+    :param int pads: The number of pad values to be inserted at the front and 
+                     back of the xi-axis. If set to None, one half of the length
+                     of the xi-axis is assumed.
+    :param float pad_value: The pad value to be inserted. Default: 0.
+    :param interpolation: A value accepted by scipy.interpolate.interp1d as 
+                          kind argument.
+    :param thetas_axis: Specifies the position of the theta-axis in sino.
+    :param xis_axis: Specifies the position of the xi-axis in sino.
+    '''
+    if not thetas:  # Assume projections over 2 pi.
+        thetas = numpy.arange(0., 2.*numpy.pi, 2.*numpy.pi / sino.shape[thetas_axis])
+    if not xis:
+        xis = numpy.arange(sino.shape[xis_axis]) - .5 * (sino.shape[xis_axis] - 1)+xi_offs
+    if not xy:
+        xi_max = numpy.max(numpy.abs(xis))
+        xi_diff_max = numpy.max(numpy.diff(xis))
+        n_coords = int(numpy.round(2.*xi_max / xi_diff_max))
+        coords = numpy.arange(n_coords) - .5 * (n_coords - 1)
+        xy = numpy.meshgrid(coords, coords)
+    if not pads:
+        pads = int(.5 * sino.shape[xis_axis])
+    if not kernel:
+        kernel = fbp.ramp()
+    sino_filtered = fbp.unpad(fbp.filter_projections(fbp.pad(sino, pads, pad_value, xis_axis), kernel, xis_axis), pads, xis_axis)
+    x, y = xy
+    return pycon.util.apply_along_axes(fbp.backproject, (thetas_axis, xis_axis),
+                                       sino_filtered, thetas, xis, x, y, interpolation)
+
--- pycon/tomo/fbp.py
+++ pycon/tomo/fbp.py
@@ -13,41 +13,43 @@ import kernel
 def xi(x, y, theta):
     return x*numpy.cos(theta)-y*numpy.sin(theta)
 
-def zero_pad(array, pads=0., value=0., axis=None):
-    array = numpy.array(array)
+def pad(arr, pads=0, value=0., axis=None):
+    pad_arr = value*numpy.ones(int(pads))
+    def _pad(subarr):
+        return numpy.concatenate((pad_arr, subarr, pad_arr))
     if axis == None:
-        axis = array.ndim-1
-    return numpy.insert(array, pads*[array.shape[axis]]+pads*[0], value,
-                        axis=axis)
+        axis = arr.ndim-1    
+    return numpy.apply_along_axis(_pad, axis, arr)
+    
+def unpad(arr, pads=0, axis=None):
+    arr = numpy.array(arr)
+    if axis == None:
+        axis = arr.ndim-1
+    if axis < 0:
+        axis = arr.ndim+axis
+    return numpy.rollaxis(numpy.rollaxis(arr, axis)[pads:-pads], 0, axis+1)
 
-def ramp(c):
+def ramp(c=1.):
     return lambda n: kernel.ramp(n, c)
 
-def hilbert(c):
+def hilbert(c=1.):
     return lambda n: kernel.hilbert(n, c)
 
-def filter_parallel_projections(array, kernel=ramp(1.), axis=None):
+def filter_projections(array, kernel=ramp(1.), axis=None):
     if axis == None:
         axis = array.ndim-1
-    print axis
-    print array.shape
-    print array.shape[axis]
     return numpy.real(fft.ifft(fft.fft(array, axis=axis)
                                *kernel(array.shape[axis]), axis=axis))
 
-def backproject_parallel_projection(projection, theta, xis, xs, ys,
+def backproject_projection(projection, theta, xis, xs, ys,
                                     kind='linear'):
     return sip.interp1d(xis, projection, fill_value=0, copy=False, kind=kind,
                       bounds_error=False, assume_sorted=True)(xi(xs, ys, theta))
 
-def backproject_parallel(sinogram, thetas, xis, xs, ys, interpolation='linear'):
-    crosssection = None
+def backproject(sinogram, thetas, xis, xs, ys, interpolation='linear'):
+    crosssection = numpy.zeros_like(xs)
     for projection, theta in zip(sinogram, thetas):
-        backprojection = backproject_parallel_projection(projection, theta, xis,
+        crosssection += backproject_projection(projection, theta, xis,
                                                      xs, ys, kind=interpolation)
-        if crosssection == None:
-            crosssection = backprojection
-        else:
-            crosssection += backprojection
     return crosssection/len(thetas)
         
