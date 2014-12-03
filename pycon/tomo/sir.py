6dcb4f8a20199ec1b70835669480324d7e151def
André Ritter <andre.ritter@fau.de>
2014-12-03 14:13:11 +0100

    In pycon.tomo.sir: Updated method get_projectors to reflect changes in _fp.

--- pycon/tomo/sir.py
+++ pycon/tomo/sir.py
@@ -18,14 +18,14 @@ pitch1_default = .1
 angles_default = 101
 
 
-thetas = numpy.arange(0., 2.*numpy.pi, 2.*numpy.pi/angles_default)
-xis = numpy.arange(-.45+.025, .5, .1)
+thetas = numpy.arange(0., 2.*numpy.pi, 2.*numpy.pi / angles_default)
+xis = numpy.arange(-.45 + .025, .5, .1)
 
-def get_projectors(thetas, xis, xipitch, n0=n0_default, n1=n1_default, pitch0=pitch0_default, pitch1=pitch1_default, offset0=0., offset1=0.):
+def get_projectors(thetas, xis, xipitch, n0=n0_default, n1=n1_default, pitch0=pitch0_default, pitch1=pitch1_default, offset0=0., offset1=0., n_mean=10):
     width0 = n0 * pitch0
     width1 = n1 * pitch1
-    p = _fp.projector_siddon2d(thetas, xis, n0, n1, width0, width1, offset0, offset1)
-    p_diff = _fp.projector_siddon2d(thetas, xis, n0, n1, width0, width1, offset0, offset1, xis_diff=xipitch*numpy.ones_like(xis)) 
+    p = _fp.projector_siddon2d(thetas, xis, n0, n1, width0, width1, offset0, offset1, xi_mean=xipitch, xi_n=n_mean)
+    p_diff = _fp.projector_siddon2d(thetas, xis, n0, n1, width0, width1, offset0, offset1, xi_diff=xipitch, xi_mean=xipitch, xi_n=n_mean) 
     p_re = p.transposed()
     p_diff_re = p_diff.transposed()
     return ((p, p_re), (p_diff, p_diff_re), (thetas, xis), (n0, n1))
@@ -42,41 +42,41 @@ def reshape_volume(vol, n0, n1):
     
 def phantom_boxes(n0=n0_default, n1=n1_default, n0_border=2, n1_border=2, vmax=(1., 1., 1.)):
     volume = empty_volume()
-    volume[0,1:11,1:6] = 1.*vmax[0]
-    volume[0,1:11,6:11] = .5*vmax[0]
-    volume[1,1:6,1:11] = 1.*vmax[1]
-    volume[1,6:11,1:11] = .5*vmax[1]
-    volume[2,2:5,2:5] = 1.*vmax[2]
-    volume[2,7:10,7:10] = .5*vmax[2]
+    volume[0, 1:11, 1:6] = 1.*vmax[0]
+    volume[0, 1:11, 6:11] = .5 * vmax[0]
+    volume[1, 1:6, 1:11] = 1.*vmax[1]
+    volume[1, 6:11, 1:11] = .5 * vmax[1]
+    volume[2, 2:5, 2:5] = 1.*vmax[2]
+    volume[2, 7:10, 7:10] = .5 * vmax[2]
     return volume
     
-def phantom_random(n0=n0_default, n1=n1_default, n0_border=2, n1_border=2, bounds=((0.,1.),(0.,1.),(0.,1.))):
+def phantom_random(n0=n0_default, n1=n1_default, n0_border=2, n1_border=2, bounds=((0., 1.), (0., 1.), (0., 1.))):
     volume = empty_volume(n0, n1)
     for i in range(3):
-        volume[i,n1_border:-n1_border,n0_border:-n0_border] = numpy.random.uniform(bounds[i][0], bounds[i][1], (n1-2*n1_border, n0-2*n0_border))
+        volume[i, n1_border:-n1_border, n0_border:-n0_border] = numpy.random.uniform(bounds[i][0], bounds[i][1], (n1 - 2 * n1_border, n0 - 2 * n0_border))
     return volume
     
 def forward_project(volume, projectors):
     volume = reshape_volume(volume, *projectors[3])
     return numpy.array([numpy.exp(-projectors[0][0].project(volume[0])),
-                        .5*projectors[1][0].project(volume[1]),
+                        .5 * projectors[1][0].project(volume[1]),
                         numpy.exp(-projectors[0][0].project(volume[2]))])
                 
 def volume_project(projection, projectors):
     return numpy.array([projectors[0][1].project(projection[0]),
-                        .5*projectors[1][1].project(projection[1]),
+                        .5 * projectors[1][1].project(projection[1]),
                         projectors[0][1].project(projection[2])])
     
 def get_phis(phi, phi0, psteps):
-    return numpy.repeat(phi[numpy.newaxis], len(psteps), axis=0)+psteps[:,numpy.newaxis,numpy.newaxis]+phi0
+    return numpy.repeat(phi[numpy.newaxis], len(psteps), axis=0) + psteps[:, numpy.newaxis, numpy.newaxis] + phi0
     
 def stepping(N0, V0, projection, phi0, psteps):
     phis = get_phis(projection[1], phi0, psteps)
-    N = N0*projection[0]*(1.+V0*projection[2]*numpy.cos(phis))
+    N = N0 * projection[0] * (1. + V0 * projection[2] * numpy.cos(phis))
     return N
     
 def loglikelihood(N, N_exp):
-    return numpy.sum(N_exp-N*numpy.log(N_exp)+N*numpy.log(N)-N)
+    return numpy.sum(N_exp - N * numpy.log(N_exp) + N * numpy.log(N) - N)
     
 def ll(volume, N, N0, V0, phi0, projectors, psteps):
     N_exp = stepping(N0, V0, forward_project(volume, projectors), phi0, psteps)
@@ -86,18 +86,18 @@ def llgradient(volume, N, N0, V0, phi0, projectors, psteps):
     projection = forward_project(volume, projectors)
     phis = get_phis(projection[1], phi0, psteps)
     C = numpy.cos(phis)
-    T = N0*projection[0]
-    TD = T*V0*projection[2]
-    N_exp = T+TD*C
-    N_ratio = numpy.nan_to_num(1.-N/N_exp)
+    T = N0 * projection[0]
+    TD = T * V0 * projection[2]
+    N_exp = T + TD * C
+    N_ratio = numpy.nan_to_num(1. - N / N_exp)
     return -volume_project([
-        numpy.sum(N_exp-N, axis=0),
-        TD*numpy.sum(N_ratio*numpy.sin(phis), axis=0),
-        TD*numpy.sum(N_ratio*C, axis=0)],
+        numpy.sum(N_exp - N, axis=0),
+        TD * numpy.sum(N_ratio * numpy.sin(phis), axis=0),
+        TD * numpy.sum(N_ratio * C, axis=0)],
         projectors)
         
 def ll3eps(eps, volume, grad, N, N0, V0, phi0, projectors, psteps):
-    return ll(volume+numpy.abs(numpy.array(eps))[:,numpy.newaxis,numpy.newaxis], N, N0, V0, phi0, projectors, psteps)
+    return ll(volume + numpy.abs(numpy.array(eps))[:, numpy.newaxis, numpy.newaxis], N, N0, V0, phi0, projectors, psteps)
         
 fmin_methods = dict(
     cg=opt.fmin_cg,
@@ -107,13 +107,13 @@ fmin_methods = dict(
 def reconstruct(N, N0, V0, phi0, projectors, psteps, method='bfgs',
                 gtol=1e-5, full_output=False, retall=False):
     start = empty_volume(*projectors[3])
-    norm = 1./abs(ll(start, N, N0, V0, phi0, projectors, psteps))
+    norm = 1. / abs(ll(start, N, N0, V0, phi0, projectors, psteps))
     ret = fmin_methods[method](
-                    lambda *args: norm*ll(*args),
-                    #ll,
+                    lambda *args: norm * ll(*args),
+                    # ll,
                     start,
-                    lambda *args: norm*llgradient(*args).flatten(),
-                    #lambda *args: llgradient(*args).flatten(),
+                    lambda *args: norm * llgradient(*args).flatten(),
+                    # lambda *args: llgradient(*args).flatten(),
                     args=(N, N0, V0, phi0, projectors, psteps),
                     gtol=gtol,
                     full_output=full_output,
@@ -124,14 +124,14 @@ def reconstruct(N, N0, V0, phi0, projectors, psteps, method='bfgs',
         return reshape_volume(ret, *projectors[3])
         
 def iterate(volume, N, N0, V0, phi0, projectors, psteps, method='cg'):
-    norm = 1./abs(ll(volume, N, N0, V0, phi0, projectors, psteps))
-    grad = norm*llgradient(volume, N, N0, V0, phi0, projectors, psteps)
+    norm = 1. / abs(ll(volume, N, N0, V0, phi0, projectors, psteps))
+    grad = norm * llgradient(volume, N, N0, V0, phi0, projectors, psteps)
     eps = fmin_methods[method](
-            lambda *args: norm*ll3eps(*args)-1.,
-            [0,0,0],
+            lambda *args: norm * ll3eps(*args) - 1.,
+            [0, 0, 0],
             args=(volume, grad, N, N0, V0, phi0, projectors, psteps)
         )
-    return volume+numpy.abs(numpy.array(eps))[:,numpy.newaxis,numpy.newaxis]*grad, eps
+    return volume + numpy.abs(numpy.array(eps))[:, numpy.newaxis, numpy.newaxis] * grad, eps
     
     
 def show_volume(volume):
@@ -145,15 +145,15 @@ def show_volume(volume):
     
 def write_movie(fprefix, data, vmin, vmax, scale=(1., 1., 1.), fps=15, n0=n0_default, n1=n1_default):
     def frame_data(frame_number):
-        return numpy.reshape([scale[0]*data[frame_number,0,:,:].T,
-                scale[1]*data[frame_number,1,:,:].T,
-                scale[2]*data[frame_number,2,:,:].T], (3*n1,n0)).T
+        return numpy.reshape([scale[0] * data[frame_number, 0, :, :].T,
+                scale[1] * data[frame_number, 1, :, :].T,
+                scale[2] * data[frame_number, 2, :, :].T], (3 * n1, n0)).T
     for i in range(len(data)):
-        pl.imsave(fprefix+'_frame_{:0=3}.png'.format(i), frame_data(i), vmin=vmin, vmax=vmax, cmap=pl.cm.gray, format='png')
+        pl.imsave(fprefix + '_frame_{:0=3}.png'.format(i), frame_data(i), vmin=vmin, vmax=vmax, cmap=pl.cm.gray, format='png')
     
 def timeit(func, *args, **kwargs):
     t_s = time.clock()
     ret = func(*args, **kwargs)
     t_e = time.clock()
-    print 'Time needed: '+str(t_e-t_s)+' s'
+    print 'Time needed: ' + str(t_e - t_s) + ' s'
     return ret
